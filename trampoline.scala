25e8164bd53565d8b0d3b6ffaaaa9ea5e352ed1c
nmcb <marco.borst@splatter.nl>
2015-09-10 19:20:19 +0200

    more idiomatic variable names

--- trampoline.scala
+++ trampoline.scala
@@ -1,13 +1,13 @@
 object trampoline {
     
-    // Non-tail recursive ackerman, with inner recursion, no tailrec implementation exists
+    // non-tail recursive ackerman; with inner recursion, no tailrec implementation exists
     def ackermann(m: Int, n: Int): Int = (m, n) match {
         case (0, _)            => n + 1
         case (_, 0) if (m > 0) => ackermann(m - 1, 1)
         case (_, _)            => ackermann(m - 1, ackermann(m, n - 1))  
     }
 
-    // Tail recursive ackerman trampoline chain, inlining possible as we support map and flatMap
+    // tail recursive ackerman trampoline chain, inlining possible as we support map and flatMap
     
     // native scala implementation
     import scala.util.control.TailCalls._ 
@@ -26,16 +26,16 @@ object trampoline {
         final def result: A = this match {
             case Done(a)    => a
             case Call(t)    => t().result
-            case Cont(a, f) => a match {
-                case Done(v)    => f(v).result
-                case Call(t)    => t().flatMap(f).result
-                case Cont(b, g) => b.flatMap(x => g(x) flatMap f).result
+            case Cont(s, c) => s match {
+                case Done(v)      => c(v).result
+                case Call(t)      => t().flatMap(c).result
+                case Cont(ss, cc) => ss.flatMap(x => cc(x) flatMap c).result
             }
         }
 
         def flatMap[B](f: A => Tramp[B]): Tramp[B] = this match {
-            case Cont(a, g) => Cont(a, (x: Any) => g(x) flatMap f)
-            case x          => Cont(x, f)
+            case Cont(s, c) => Cont(s, (x: Any) => c(x) flatMap f)
+            case c          => Cont(c, f)
         }
 
         def map[B](f: A => B): Tramp[B] = flatMap(a => Done(f(a)))
@@ -43,7 +43,7 @@ object trampoline {
 
     case class Done[+A]    (a: A)                          extends Tramp[A]
     case class Call[+A]    (t: () => Tramp[A])             extends Tramp[A]
-    case class Cont[A, +B] (s: Tramp[A], f: A => Tramp[B]) extends Tramp[B]
+    case class Cont[A, +B] (s: Tramp[A], c: A => Tramp[B]) extends Tramp[B]
     
     def tdone[A](a: A)           = Done(a)
     def tcall[A](t: => Tramp[A]) = Call(() => t)
